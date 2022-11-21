523850a9e3c3bd2312f3f758e08b3a478a59cd03
zedwong <kingflyme1@gmail.com>
2022-11-21 12:36:31 +0900

    add skeleton

--- src/lib/components/pool/PoolIcon.svelte
+++ src/lib/components/pool/PoolIcon.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+    import Skeleton from "../common/skeleton.svelte";
+
   export let logos: Array<string>;
 </script>
 
 {#each logos as uri}
   <div class="avatar rounded-full w-10">
     <img src={uri} alt="Token icon" />
+    <!-- <Skeleton height=30 width=40 /> -->
   </div>
 {/each}
 
