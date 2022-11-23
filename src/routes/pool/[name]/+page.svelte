ba66cf0b2b971af7435da4b1b9a335bd8df9d94b
zedwong <kingflyme1@gmail.com>
2022-11-23 18:49:13 +0900

    add poolinfo

--- src/routes/pool/[name]/+page.svelte
+++ src/routes/pool/[name]/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import Deposit from "../elements/Deposit.svelte";
+  import PoolInfo from "../elements/PoolInfo.svelte";
 
   console.log($page.params.name);
 </script>
 
 <div>
   <Deposit />
+  <PoolInfo />
 </div>
---
+++ src/routes/pool/elements/PoolInfo.svelte
@@ -0,0 +1,13 @@
+<div class="card max-w-sm min-w-[40rem] w-96 bg-base-100 shadow-xl ml-4 h-96">
+  <div>
+    <!-- Title -->
+  </div>
+
+  <div>
+    <!-- Pool Balance -->
+  </div>
+
+  <div>
+    <!-- APY -->
+  </div>
+</div>
