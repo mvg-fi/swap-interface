143ad7bb2ec849c0e9288c26cb31c4728ad74045
zedwong <kingflyme1@gmail.com>
2022-11-23 17:12:51 +0900

    add add_liquidity title

--- src/routes/pool/elements/Deposit.svelte
+++ src/routes/pool/elements/Deposit.svelte
@@ -2,11 +2,15 @@
   import Button from "./deposit/button.svelte";
   import Infos from "./deposit/infos.svelte";
   import Input from "./deposit/input.svelte";
+  import Title from "./deposit/title.svelte";
 </script>
 
 <div class="card max-w-sm min-w-[23rem] w-96 bg-base-100 shadow-xl">
   <div class="flex flex-col">
-    <div class="my-3 mx-2">
+    <div class="my-3 mb-2">
+      <Title />
+    </div>
+    <div class="mx-2">
       <Input />
     </div>
     <div>
---
+++ src/routes/pool/elements/deposit/title.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import Refresh from "$lib/images/refresh.svg";
+
+  const switchToWithdraw = () => {
+    console.log('TODO, switch to withdraw')
+  }
+</script>
+
+<div class="flex text-center justify-center">
+  <span class="font-bold text-base"> {$_("add_liquidity.deposit")} </span>
+  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <img
+    alt=""
+    tabindex="0"
+    src={Refresh}
+    on:click={()=>switchToWithdraw()}
+    class="w-3 ml-2 mb-0.5 cursor-pointer"
+  />
+</div>
+
+<style>
+  img:focus {
+    animation: shake 1s;
+    animation-iteration-count: 1;
+  }
+
+  @keyframes shake {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(360deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
