5fcb1dbbfe3e9d07ea6a8c8465853946cc66d0f5
zedwong <kingflyme1@gmail.com>
2023-12-09 20:34:12 +0700

    add menu for mm

---
+++ src/lib/components/navbar/mobileMenuDialog.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import { themeChange } from "theme-change";
+  $: items = [
+    { key: $_("dark_mode"), value: false, check: true, func: ()=>{themeChange(true)} },
+    { key: $_("language"), value: "", check: false, func: ()=>{} },
+    { key: $_("currency"), value: "", check: false, func: ()=>{} },
+  ];
+</script>
+
+<dialog id="mobile_menu_dialog" class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg mb-3">Settings</h3>
+    {#each items as { key, value, check, func }}
+      <button class="w-full flex items-center justify-between" on:click={func()}>
+        <p class="py-3">{key}</p>
+        {#if check}
+          <input type="checkbox" class="toggle" bind:checked={value} />
+        {:else}
+          <!-- chevron right -->
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>        
+        {/if}
+        </button>
+    {/each}
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -8,6 +8,7 @@
   import Account from "./account.svelte";
   import { isMixin } from "$lib/helpers/mixin";
   import { connected } from "$lib/stores/connect";
+  import MobileMenuDialog from "./mobileMenuDialog.svelte";
 
   const ism = isMixin();
 </script>
@@ -48,7 +49,7 @@
     <div class="pl-2 mt-0.5">
       <Logo class="w-6" />
       <div class="absolute right-28 flex">
-        <button on:click={()=>{}}>
+        <button on:click={()=>{mobile_menu_dialog.showModal()}}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
           </svg>  
@@ -56,4 +57,5 @@
       </div>
     </div>
   </div>
+  <MobileMenuDialog />
 {/if}
