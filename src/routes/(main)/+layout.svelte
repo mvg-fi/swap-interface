9c441959cd6de625339043a3f811acae19db6131
zedwong <kingflyme1@gmail.com>
2023-12-12 09:28:41 +0700

    fix duplicate i18n init

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,42 +1,32 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import Loading from "../loading.svelte";
-  import { assets} from "$lib/stores/asset";
+  import { assets } from "$lib/stores/asset";
   import type { Asset } from "$lib/types/asset";
   import Navbar from "$lib/components/navbar/navbar.svelte";
 
   import "../../app.postcss";
   import "../styles.css";
-  import { initi18n } from "../../i18n/i18n";
   import BottomNav from "$lib/components/footer/bottomNav.svelte";
 
   let a: Asset[] | undefined = $page.data.assets;
   a?.length && !$assets.length && assets.set(a);
   $: a?.length && !$assets.length && assets.set(a);
-
-  const setupFn = async () => {
-    await initi18n();
-  };
-  const setup = setupFn();
 </script>
 
-{#await setup}
-  <Loading />
-{:then}
-  <div class="text-base-content">
-    <header class="sticky top-0 z-10 backdrop-blur-sm">
-      <Navbar />
-    </header>
+<div class="text-base-content">
+  <header class="sticky top-0 z-10 backdrop-blur-sm">
+    <Navbar />
+  </header>
 
-    <main class="!px-2">
-      <slot />
-    </main>
+  <main class="!px-2">
+    <slot />
+  </main>
 
-    <footer>
-      <BottomNav />
-    </footer>
-  </div>
-{/await}
+  <footer>
+    <BottomNav />
+  </footer>
+</div>
 
 <style>
   main {
