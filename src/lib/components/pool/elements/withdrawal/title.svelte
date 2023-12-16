0a7ab0de188e7e54cdae9e9a8aa56f9ac0ee66e9
zedwong <kingflyme1@gmail.com>
2023-12-16 22:14:16 +0700

    fix typo

--- src/lib/components/pool/elements/withdrawal/title.svelte
+++ src/lib/components/pool/elements/withdrawal/title.svelte
@@ -11,6 +11,6 @@
   <span class="font-bold text-base -translate-y-[0.5px]"> {$_("add_liquidity.withdraw")} </span>
   <span class="px-2"> / </span>
   <button on:click={()=>setDepositMode(2)}>
-    <span class="cursor-pointer text-sm"> {$_("add_liquidity.staking")} </span>
+    <span class="cursor-pointer text-sm"> {$_("add_liquidity.stake")} </span>
   </button>
 </div>
